f54f18207db7003a9c9b044f6a12f116e432148d
Marianela <masham1860@gmail.com>
2022-12-01 18:44:26 -0300

    s

---
+++ controllers/detalle-consola-controller.js
@@ -0,0 +1,43 @@
+import { consolaServices } from "../servicios/consola-servicios.js";
+import { formatPrice } from "../formatterPrices.js";
+
+
+const getDetalleConsola = (name, price, imageUrl, id) => {
+    const card = document.createElement("div");
+  
+    const contenido = `
+      <div class="produts__details" id=${id}>
+      
+          
+          <img src="${imageUrl}" alt="img">
+          <h1 class="product-name"> ${name} </h1>
+          <p class="preco">${formatPrice(price)}</p>
+      </div>
+      `;
+    card.innerHTML = contenido;
+    card.dataset.id = id;
+    return card;
+  };
+
+  const consola = document.querySelector("[data-detalleConsola"); 
+
+ const render = async () => {
+    try {
+      const detalleConsola = await consolaServices.listarUnaConsola();
+  
+      detalleConsola.forEach((producto) => {
+        consola.appendChild(
+          getDetalleConsola(
+            producto.name,
+            producto.price,
+            producto.imageUrl,
+            producto.id
+          )
+        );
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  render();

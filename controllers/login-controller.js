c08f1278ab50b9c7621f9f827bed67a43b4b0044
Marianela <masham1860@gmail.com>
2022-11-30 21:35:40 -0300

    viendo

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -7,10 +7,24 @@ form.addEventListener("submit", (evento) => {
   const email = document.querySelector("[data-email]").value;
   const password = document.querySelector("[data-password]").value;
 
-  
-  if ((email && password)==loginService.login(email,password)) {
-    window.location.href = "../screens/produto.html";
-  } else {
-    alert("email o contraseña incorrectos");
-  }
-});
+
+  let valid = false;
+
+loginService.login()
+    .then((data) => {
+        data.forEach((user) => {
+            if(email === user.email && password === user.password) {
+                valid = true;
+            }
+        }); 
+        
+        if(valid) {
+            
+            window.location.href = "../screens/produto.html";
+        } else {
+            alert('Combinación email-password inválida')
+        }
+    })
+    .catch((error) => alert('Ocurrio un error, intente más tarde.'));    
+
+})
--- servicios/usuario-servicio.js
+++ servicios/usuario-servicio.js
@@ -1,20 +1,7 @@
-const login = async (email, password) => {
-    const resposta = await fetch(`https://challenge4.onrender.com/users`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email,
-            password
-        }),
-    });
-    if (resposta.ok) {
-        return resposta.body;
-    }
-    throw new Error("ERROR, email o contraseña inválido");
-  };
+const loginUsuario = () => fetch(`https://challenge4.onrender.com/users`).then((respuesta) => respuesta.json());
+
+      
 
   export const loginService = {
-    login
+    loginUsuario
   }

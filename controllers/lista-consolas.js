04d039a2dd19b914fbb8cde7b0b4c8172b1d711f
Marianela <masham1860@gmail.com>
2022-12-01 17:10:30 -0300

    s

--- controllers/lista-consolas.js
+++ controllers/lista-consolas.js
@@ -1,4 +1,5 @@
 import { consolaServices} from "../servicios/consola-servicios.js"
+import {productoServices}  from "../servicios/producto-servicios.js"
 
 
 import { formatPrice } from "../formatterPrices.js";
@@ -31,8 +32,37 @@ const getConsolas = (name, price, imageUrl, id) => {
   card.dataset.id = id;
   return card;
 };
+const getProducts = (name, price, imageUrl, id) => {
+  const card = document.createElement("div");
+
+  const contenido = `
+    <div class="produto">
+        <div class="container">
+            <button class="buttonDelete" type="button">
+              <img class="deleteImage" src="../assets/delete.png" alt="Deletar" />
+            </button>
+            
+            <a href="../screens/edit-product.html?id=${id}">
+            
+              <button class="buttonEdit" type="button">
+                <img class="editImage" src="../assets/edit.png" alt="Editar" />
+              </button>
+            
+            </a>
+        </div>
+        
+        <img src="${imageUrl}" alt="img">
+        <h1 class="product-name"> ${name} </h1>
+        <p class="preco">${formatPrice(price)}</p>
+    </div>
+    `;
+  card.innerHTML = contenido;
+  card.dataset.id = id;
+  return card;
+};
 
 const consolas = document.querySelector("[data-allProducts]");
+const productos = document.querySelector("[data-allProducts]");
 
 consolas.addEventListener("click", async (evento) => {
   let deleteButton = evento.target.className === "deleteImage";
@@ -49,6 +79,23 @@ consolas.addEventListener("click", async (evento) => {
   }
 });
 
+productos.addEventListener("click", async (evento) => {
+  let deleteButton = evento.target.className === "deleteImage";
+  if (deleteButton) {
+    const producto = evento.target.closest("[data-id]");
+    let id = producto.dataset.id;
+    productoServices
+      .deleteProducto(id)
+      .then((res) => {
+        producto.remove();
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+
+
 const render = async () => {
   try {
     const listaConsolas = await consolaServices.listaConsolas();
@@ -63,6 +110,19 @@ const render = async () => {
         )
       );
     });
+
+        const listaProductos = await productoServices.listaProductos();
+    
+        listaProductos.forEach((producto) => {
+          productos.appendChild(
+            getProducts(
+              producto.name,
+              producto.price,
+              producto.imageUrl,
+              producto.id
+            )
+          );
+        });
   } catch (err) {
     console.log(err);
   }

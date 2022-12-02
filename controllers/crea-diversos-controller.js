922ff667d20605e48265017cd5bfff9391dd365e
Marianela <masham1860@gmail.com>
2022-12-02 19:10:46 -0300

    mmm

--- controllers/crea-diversos-controller.js
+++ controllers/crea-diversos-controller.js
@@ -8,8 +8,9 @@ form.addEventListener("submit", (evento) => {
   const nome = document.querySelector("[data-nome]").value;
   const url = document.querySelector("[data-url]").value;
   const preco = document.querySelector("[data-preco]").value;
+  const categ = document.querySelector("[data-diverso]").value;
 
-  diversoServices.creaDiverso(nome, url, preco)
+  diversoServices.creaDiverso(nome, url, preco,categ)
     .then((resposta) => {
       window.location.href = "../screens/produto.html";
       console.log(resposta);
--- controllers/crea-productos-controller.js
+++ controllers/crea-productos-controller.js
@@ -8,9 +8,10 @@ form.addEventListener("submit", (evento) => {
   const nome = document.querySelector("[data-nome]").value;
   const url = document.querySelector("[data-url]").value;
   const preco = document.querySelector("[data-preco]").value;
+  const categ = document.querySelector("[data-producto]").value;
 
   productoServices
-    .creaProdutos(nome, url, preco)
+    .creaProdutos(nome, url, preco,categ)
     .then((resposta) => {
       window.location.href = "../index.html";
       console.log(resposta);
